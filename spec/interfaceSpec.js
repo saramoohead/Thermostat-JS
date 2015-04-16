683cc874e02971430e95df4a9cd380ffa9a2d5ec
Sara OC <saramoo@hotmail.com>
2015-04-16 17:27:37 +0100

    power save mode toggles

--- spec/interfaceSpec.js
+++ spec/interfaceSpec.js
@@ -1,43 +1,47 @@
-describe('Interface', function() {
+describe('Interface', function () {
 
-  beforeEach(function(){
-      jasmine.getFixtures().fixturesPath = '../';
-      loadFixtures('index.html');
-  });
-
-  describe('can display', function() {
-
-    it('default temperature', function() {
-      expect($('.temperature')).toContainText('20');
+    beforeEach(function () {
+        jasmine.getFixtures().fixturesPath = '../';
+        loadFixtures('index.html');
     });
 
-    it('an increased temperature when the up arrow is clicked', function () {
-      $('input.up').click();
-      expect($('.temperature')).toContainText('21');
+    describe('can display', function () {
+
+        it('default temperature', function () {
+            expect($('.temperature')).toContainText('20');
+        });
+
+        it('an increased temperature when the up arrow is clicked', function () {
+            $('input.up').click();
+            expect($('.temperature')).toContainText('21');
+        });
+
+        it('a decreased temperature when the down arrow is clicked', function () {
+            $('input.down').click();
+            expect($('.temperature')).toContainText('19');
+        });
+
+        it('has a maximum of 25 when power save mode is on', function () {
+            expect($('input.psmode')).toBeChecked();
+                for (var i=0; i<7; i++) {
+            $('input.up').click();
+            }
+            expect($('.temperature')).toContainText('25');
+        });
+
+        it('has a maximum of 32 when power save mode is off', function() {
+            $('input.psmode').click();
+            expect($('input.psmode')).not.toBeChecked();
+                for(var i=0; i<15; i++) {
+            $('input.up').click();
+            }
+            expect($('.temperature')).toContainText('32');
+        });
+
     });
 
-    it('a decreased temperature when the down arrow is clicked', function () {
-      $('input.down').click();
-      expect($('.temperature')).toContainText('19');
-    });
+  // test colour
 
-    it('has a maximum of 25 when power save mode is on', function () {
-      expect($('input.psmode')).toBeChecked();
-      for(var i=0; i<7; i++) {
-        $('input.up').click();
-      }
-      expect($('.temperature')).toContainText('25');
-    });
-
-    it('has a maximum of 32 when power save mode is off', function() {
-      $('input.psmode').click();
-      expect($('input.psmode')).not.toBeChecked();
-      for(var i=0; i<15; i++) {
-        $('input.up').click();
-      }
-      expect($('.temperature')).toContainText('32');
-    });
-
-  });
+  // split up describe blocks
 
 });
