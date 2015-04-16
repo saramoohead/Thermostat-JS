e0b1421915ca541fdd8629202eb6558c3cbefe6b
Sara OC <saramoo@hotmail.com>
2015-04-16 14:50:43 +0100

    decrease temp with down arrow

--- spec/interfaceSpec.js
+++ spec/interfaceSpec.js
@@ -16,9 +16,17 @@ describe('Interface', function() {
       expect($('.temperature')).toContainText('21');
     });
 
-    it('a decreased temperature when the up arrow is clicked', function () {
+    it('a decreased temperature when the down arrow is clicked', function () {
       $('input.down').click();
       expect($('.temperature')).toContainText('19');
+    });
+
+    it('has a maximum when power save mode is on', function () {
+      expect($('input.psmode')).toBeChecked();
+      for(var i=0; i<7; i++) {
+        $('input.up').click();
+      }
+      expect($('.temperature')).toContainText('25');
     });
 
   });
